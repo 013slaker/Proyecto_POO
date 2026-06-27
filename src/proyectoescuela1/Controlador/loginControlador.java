0519010442bd23fd530b05fd781ba0a4c0966bc0
gisepalomares <gisepalomares@gmail.com>
2026-06-27 04:16:14 -0500

    cambio login

--- src/proyectoescuela1/Controlador/loginControlador.java
+++ src/proyectoescuela1/Controlador/loginControlador.java
@@ -1,28 +1,25 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package proyectoescuela1.Controlador;
+
 import java.util.List;
-import proyectoescuela1.Modelo.*;
+import proyectoescuela1.Modelo.Cuenta;
 
-public class loginControlador {
-  private List<Cuenta> cuentas;
+public class LoginControlador {
 
-    public loginControlador(List<Cuenta> cuentas) {
+    private List<Cuenta> cuentas;
+
+    public LoginControlador(List<Cuenta> cuentas) {
         this.cuentas = cuentas;
     }
-  //devuelve el cargo si las credenciales son correctas//
-  //si son incorrectas simplemente devuelve nulo
-  public String validarLogin(String usuario, String contrasena){
-  for(Cuenta c: cuentas){
-      if(c.login(usuario, contrasena)){
-      return c.getRol();
-      
-      }
-      
-  } 
-      
-  return null;
-  }     
+
+    // ahora devuelve la cuenta, no el rol
+    public Cuenta validarLogin(String usuario, String contrasena) {
+
+        for (Cuenta c : cuentas) {
+            if (c.login(usuario, contrasena)) {
+                return c;
+            }
+        }
+        return null;
+    }
 }
